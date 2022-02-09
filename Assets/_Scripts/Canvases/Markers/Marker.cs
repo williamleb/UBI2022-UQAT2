431a8537bd77fcd56e142dfde82719979ac93005
William Lebel <william.leb@hotmail.com>
2022-02-09 00:00:20 -0500

    Add marker test

---
+++ Assets/_Scripts/Canvases/Markers/Marker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace Canvases.Markers
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class Marker : MonoBehaviour
+    {
+        private RectTransform rectTransform;
+        private Camera camera;
+
+        private Vector3 worldPosition = Vector3.zero;
+
+        public Vector3 Position
+        {
+            get => worldPosition;
+            set => worldPosition = value;
+        }
+
+        private void Awake()
+        {
+            rectTransform = GetComponent<RectTransform>();
+        }
+
+        private void Start()
+        {
+            camera = Camera.main;
+            Debug.Assert(camera, $"The script {nameof(Marker)} needs a {nameof(Camera)} in the scene");
+        }
+
+        private void Update()
+        {
+            var screenPosition = camera.WorldToScreenPoint(worldPosition);
+            rectTransform.position = screenPosition;
+        }
+    }
+}
