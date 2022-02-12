12e186a80592f62d6d0843f13e69463936a69f27
William Lebel <william.leb@hotmail.com>
2022-02-12 18:04:22 -0500

    Create marker receptors where markers will be shown

---
+++ Assets/_Scripts/Canvases/Markers/SpriteMarkerReceptor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Canvases.Markers
+{
+    public class SpriteMarkerReceptor : MarkerReceptor<SpriteMarker>
+    {
+        [SerializeField] private Sprite sprite;
+        
+        protected override void OnActivated()
+        {
+            CurrentMarker.Sprite = sprite;
+        }
+    }
+}
