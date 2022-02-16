7aa514ca84f669501a60c269ea129d0cc69e3912
Martin Johnson <martinjohnson@live.ca>
2022-02-16 00:12:51 -0500

    Adjusted imageUI to fit other UIComponent

--- Assets/_Scripts/Canvases/Components/ImageUIComponent.cs
+++ Assets/_Scripts/Canvases/Components/ImageUIComponent.cs
@@ -1,12 +1,26 @@
-﻿using UnityEngine;
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Canvases.Components
 {
+    // ReSharper disable once InconsistentNaming Reason: UI should be capitalized
     public class ImageUIComponent : UIComponentBase
     {
+        [Header("Association")][Required]
         [SerializeField] private Image image;
 
+        private Color initialColor;
+
+        private void Awake() => initialColor = image.color;
+        
+        private void Start()
+        {
+            Debug.Assert(image, $"A {nameof(image)} must be assigned to a {nameof(ImageUIComponent)}");
+        }
+
+        public void ResetColor() => image.color = initialColor;
+        
         public Sprite Sprite
         {
             set => image.sprite = value;
@@ -17,6 +31,10 @@ namespace Canvases.Components
             set => image.color = value;
         }
 
-        private void OnValidate() => image = GetComponent<Image>();
+        private void OnValidate()
+        {
+            if (!image)
+                image = GetComponent<Image>();
+        }
     }
 }
