aa8581dc8b7a170afa1fae5e2058f111c0cec5c8
OrangeBender <martinjohnson@live.ca>
2022-02-22 21:00:54 -0500

    Merge pull request #7 from williamleb/Task/smoothMovement
    smooth movement

--- Assets/_Scripts/Scriptables/PlayerSettings.cs
+++ Assets/_Scripts/Scriptables/PlayerSettings.cs
@@ -6,46 +6,19 @@ namespace Scriptables
     public class PlayerSettings : ScriptableObject
     {
         [Header("Player movement config")] [SerializeField]
-        private float moveMaximumSpeed = 13f;
-
-        [SerializeField] private float maxFallSpeed = -40f;
-        [SerializeField] private float moveAcceleration = 90f;
-        [SerializeField] private float moveDeceleration = 60f;
-        [SerializeField] private float minFallAcceleration = 80f;
-        [SerializeField] private float maxFallAcceleration = 120f;
-
-        [Space] [Header("Mouse settings")] [SerializeField]
-        private float mouseSensitivity = 0.5f;
-
-        [Space] [Header("Jump settings")] [SerializeField]
-        private float jumpHeight = 30f;
-
-        [Tooltip("Amount of time after leaving the platform to still allow player to jump")] [SerializeField]
-        private float jumpCoyoteTimeThreshold = 0.1f;
-
-        [Tooltip("Increases the range of height which is considered the peak of the jump")] [SerializeField]
-        private float jumpApexThreshold = 10f;
-
-        [Tooltip("Gives more movement at the peak of the jump")] [SerializeField]
-        private float moveAirBonusControl = 2f;
-
-        [Tooltip("Gravity multiplier when you let go of jump button")] [SerializeField]
-        private float jumpEndEarlyGravityModifier = 3f;
+        private float moveMaximumSpeed = 7f;
+        [SerializeField] private float sprintMaximumSpeed = 14f;
+        [SerializeField] private float moveAcceleration = 100f;
+        [SerializeField] private float moveDeceleration = 7f;
+        [SerializeField] private float dashDistance = 10f;
 
         #region accessors
 
         public float MoveMaximumSpeed => moveMaximumSpeed;
+        public float SprintMaximumSpeed => sprintMaximumSpeed;
         public float MoveAcceleration => moveAcceleration;
         public float MoveDeceleration => moveDeceleration;
-        public float MoveAirBonusControl => moveAirBonusControl;
-        public float MouseSensitivity => mouseSensitivity;
-        public float JumpHeight => jumpHeight;
-        public float MinFallAcceleration => minFallAcceleration;
-        public float MaxFallAcceleration => maxFallAcceleration;
-        public float MaxFallSpeed => maxFallSpeed;
-        public float JumpCoyoteTimeThreshold => jumpCoyoteTimeThreshold;
-        public float JumpApexThreshold => jumpApexThreshold;
-        public float JumpEndEarlyGravityModifier => jumpEndEarlyGravityModifier;
+        public float DashDistance => dashDistance;
 
         #endregion
     }
--- Assets/_Scripts/Units/Player/PlayerMovement.cs
+++ Assets/_Scripts/Units/Player/PlayerMovement.cs
@@ -1,24 +1,18 @@
+using System.Collections;
 using Fusion;
 using Systems.Network;
 using UnityEngine;
-using Utilities.Extensions;
 
 namespace Units.Player
 {
+    [RequireComponent(typeof(NetworkCharacterController))]
     public partial class PlayerEntity
     {
         [SerializeField] private Transform orientation;
 
         private NetworkCharacterController cc;
 
-        [Networked] private Vector2 MoveDirection { get; set; } = Vector2.zero;
-        [Networked] private Vector2 LookDelta { get; set; }
-
-        private NetworkBool jumpInput;
-        private NetworkBool jumpImpulse;
-        private float apexPoint;
-        private float fallSpeed;
-        private NetworkBool bufferJump;
+        [Networked] private Vector3 MoveDirection { get; set; } = Vector3.zero;
 
         private void MovementAwake()
         {
@@ -26,85 +20,56 @@ namespace Units.Player
             cc.Config.MaxSpeed = data.MoveMaximumSpeed;
             cc.Config.Acceleration = data.MoveAcceleration;
             cc.Config.Braking = data.MoveDeceleration;
-            cc.Config.AirControl = true;
-            cc.Config.BaseJumpImpulse = data.JumpHeight;
-
         }
 
         private void MoveUpdate(NetworkInputData inputData)
         {
             GetInput(inputData);
-            CalculateJumpApex();
-            CalculateGravity();
-            CalculateJump();
             MovePlayer();
             RotatePlayer();
         }
 
         private void GetInput(NetworkInputData inputData)
         {
-            MoveDirection = inputData.Move;
-            LookDelta = inputData.Look;
-            if (!jumpInput && inputData.IsJump) jumpImpulse = true;
-            jumpInput = inputData.IsJump;
-            if (jumpImpulse && !cc.Grounded && cc.Velocity.y < 0) bufferJump = true;
+            MoveDirection = default;
+            if (inputData.IsUp)
+            {
+                MoveDirection += Vector3.forward;
+            }
+            else if (inputData.IsDown)
+            {
+                MoveDirection += Vector3.back;
+            }
+
+            if (inputData.IsLeft)
+            {
+                MoveDirection += Vector3.left;
+            }
+            else if (inputData.IsRight)
+            {
+                MoveDirection += Vector3.right;
+            }
+            cc.MaxSpeed = inputData.IsSprint ? data.SprintMaximumSpeed : data.MoveMaximumSpeed;
+            if (inputData.IsDash) StartCoroutine(Dash());
         }
 
-        private void CalculateJumpApex()
+        private IEnumerator Dash()
         {
-            if (!cc.Grounded)
+            for (int i = 0; i < data.DashDistance; i++)
             {
-                apexPoint = Mathf.InverseLerp(data.JumpApexThreshold, 0, Mathf.Abs(cc.Velocity.y));
-                fallSpeed = Mathf.Lerp(data.MinFallAcceleration, data.MaxFallAcceleration, apexPoint);
-            }
-            else
-            {
-                apexPoint = 0;
-            }
-        }
-
-        private void CalculateGravity()
-        {
-            if (cc.Grounded)
-            {
-                if (cc.Velocity.y < 0) cc.Velocity = Vector3.up * -0.1f;
-            }
-            else
-            {
-                if (!jumpInput && cc.Velocity.y > 0)
-                {
-                    cc.Velocity -= Vector3.up * fallSpeed * data.JumpEndEarlyGravityModifier * Time.deltaTime;
-                }
-                else
-                {
-                    cc.Velocity -= Vector3.up * fallSpeed * Time.deltaTime;
-                }
-
-                if (cc.Velocity.y < data.MaxFallSpeed) cc.Velocity += Vector3.up * (data.MaxFallSpeed - cc.Velocity.y);
-            }
-        }
-
-        private void CalculateJump()
-        {
-            if ((jumpImpulse || bufferJump) && cc.Grounded)
-            {
-                jumpImpulse = false;
-                bufferJump = false;
-                cc.Jump();
+                cc.Move(MoveDirection);
+                yield return null;
             }
         }
 
         private void MovePlayer()
         {
-            cc.Move(MoveDirection.V2ToFlatV3());
+            cc.Move(MoveDirection);
         }
 
         private void RotatePlayer()
         {
             Vector3 ori = orientation.eulerAngles;
-            ori.x = Mathf.Clamp(ori.x - LookDelta.y * data.MouseSensitivity, 1, 75);
-            ori.y += LookDelta.x * data.MouseSensitivity;
-            ori.z = 0;
             orientation.rotation = Quaternion.Euler(ori);
         }
     }
