bbcd395aa174be143e1493eefee08406063f03e7
William Lebel <william.leb@hotmail.com>
2022-02-23 16:41:48 -0500

    Keep a reference to all spawned AIs

--- Assets/_Scripts/Units/AI/AIEntity.cs
+++ Assets/_Scripts/Units/AI/AIEntity.cs
@@ -17,6 +17,8 @@ namespace Units.AI
 
         private NavMeshAgent agent;
         private AIBrain brain;
+        
+        [Networked] public bool IsTeacher { get; private set; }
 
         public NavMeshAgent Agent => agent;
 
@@ -25,12 +27,45 @@ namespace Units.AI
             agent = GetComponent<NavMeshAgent>();
         }
 
+        // Those two methods should only be called before the AI entity is spawned
+        public void MarkAsTeacher() => IsTeacher = true;
+        public void MarkAsStudent() => IsTeacher = false;
+
         public override void Spawned()
         {
             if (brainToAddOnSpawned && Object.HasStateAuthority)
             {
                 AddBrain(brainToAddOnSpawned);
             }
+
+            RegisterToManager();
+        }
+
+        public override void Despawned(NetworkRunner runner, bool hasState)
+        {
+            UnregisterToManager();
+        }
+
+        private void RegisterToManager()
+        {
+            if (!AIManager.HasInstance)
+                return;
+
+            if (IsTeacher)
+                AIManager.Instance.RegisterTeacher(this);
+            else
+                AIManager.Instance.RegisterStudent(this);
+        }
+
+        private void UnregisterToManager()
+        {
+            if (!AIManager.HasInstance)
+                return;
+
+            if (IsTeacher)
+                AIManager.Instance.UnregisterTeacher(this);
+            else
+                AIManager.Instance.UnregisterStudent(this);
         }
 
         public void AddBrain(GameObject brainPrefab)
--- Assets/_Scripts/Units/AI/AIManager.cs
+++ Assets/_Scripts/Units/AI/AIManager.cs
@@ -1,4 +1,5 @@
-﻿using Systems.Network;
+﻿using System.Collections.Generic;
+using Systems.Network;
 using Fusion;
 using UnityEngine;
 using Utilities.Singleton;
@@ -9,6 +10,31 @@ namespace Units.AI
     {
         [SerializeField] private AIEntity entityPrefab;
         [SerializeField] private GameObject brainPrefab;
+
+        private AIEntity teacher = null;
+        private List<AIEntity> students = new List<AIEntity>();
+
+        public void RegisterTeacher(AIEntity teacherAI)
+        {
+            Debug.Assert(!teacher, "Trying to assign a teacher when there is already another teacher (there can only be one teacher)");
+            teacher = teacherAI;
+        }
+
+        public void UnregisterTeacher(AIEntity teacherAI)
+        {
+            Debug.Assert(teacher == teacherAI, "Trying to unregister a teacher that was not the current teacher");
+            teacher = null;
+        }
+
+        public void RegisterStudent(AIEntity student)
+        {
+            students.Add(student);
+        }
+
+        public void UnregisterStudent(AIEntity student)
+        {
+            students.Remove(student);
+        }
 
         private void Start()
         {
@@ -27,11 +53,21 @@ namespace Units.AI
 
         private void OnPlayerJoined(NetworkRunner runner, PlayerRef playerRef)
         {
-            if (NetworkSystem.Instance.IsHost)
-            {
-                var entity = runner.Spawn(entityPrefab, Vector3.zero, Quaternion.identity,playerRef);
-                entity.AddBrain(brainPrefab);
-            }
+            if (!NetworkSystem.Instance.IsHost) 
+                return;
+            
+            
+            var entity = runner.Spawn(entityPrefab, Vector3.zero, Quaternion.identity, playerRef,
+                (networkRunner, aiObject) =>
+                {
+                    var entity = aiObject.GetComponent<AIEntity>();
+                    Debug.Assert(entity, $"An AI must have a {nameof(AIEntity)} attached");
+
+                    var homeworkHandingStation = aiObject.GetComponentInChildren<HomeworkHandingStation>();
+                    if (homeworkHandingStation) entity.MarkAsTeacher();
+                    else entity.MarkAsStudent();
+                });
+            entity.AddBrain(brainPrefab);
         }
     }
 }
