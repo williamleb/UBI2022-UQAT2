41c24ad1e9f96d58fbcf1bac8a58bb2e53233bd5
William Lebel <william.leb@hotmail.com>
2022-02-23 00:58:38 -0500

    Update prompts with current device

--- Assets/_Scripts/Canvases/Prompts/Prompt.cs
+++ Assets/_Scripts/Canvases/Prompts/Prompt.cs
@@ -1,39 +1,99 @@
-﻿using System;
-using InputSystem;
+﻿using System.Threading.Tasks;
+using Canvases.Components;
+using Fusion;
+using Units.Player;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Utilities;
 
 namespace Canvases.Prompts
 {
-    [RequireComponent(typeof(PlayerInput))]
+    [RequireComponent(typeof(ImageUIComponent))]
+    [RequireComponent(typeof(CanvasGroup))]
     class Prompt : MonoBehaviour
     {
-        private PlayerInput playerInput;
-        private DetectDevice detectDevice;
+        [SerializeField] private string debugAction = ""; // TODO Remove
+        
+        private InputAction action = null;
+        private string inputDevice = "";
+        
+        private PlayerInputHandler playerInputHandler;
+        
+        private ImageUIComponent image;
+        private CanvasGroup canvasGroup;
+
+        public string Action
+        {
+            set
+            {
+                action = FindActionForActionName(value);
+                UpdateIcon();
+            }
+        }
 
         private void Awake()
         {
-            playerInput = playerInput;
+            image = GetComponent<ImageUIComponent>();
+            canvasGroup = GetComponent<CanvasGroup>();
         }
 
         private void Start()
         {
-            detectDevice = new DetectDevice(playerInput.currentActionMap);
+            // We wait for the player to be spawned before having access to the inputs bindings
+            // The downside to this is that we cannot show prompts when the player isn't spawned
+            PlayerEntity.OnPlayerSpawned += Init;
+            
+            UpdateIcon();
         }
 
-        private void OnEnable()
+        private void OnDestroy()
         {
-            detectDevice.OnInputDeviceChanged += OnInputDeviceChanged;
+            if (playerInputHandler)
+            {
+                playerInputHandler.OnInputDeviceChanged -= OnInputDeviceChanged;
+            }
         }
 
-        private void OnDisable()
+        private async void Init(NetworkObject player)
         {
-            detectDevice.OnInputDeviceChanged -= OnInputDeviceChanged;
+            // It's the same hack that is used in RebindUI.Init so we can know when the player is initialized.
+            // If we find a better way to know detect that in RebindUI.Init, we should also change it here
+            await Task.Delay(100);
+
+            if (!player || !player.HasInputAuthority) 
+                return;
+            
+            playerInputHandler = player.GetComponent<PlayerInputHandler>();
+            Debug.Assert(playerInputHandler);
+
+            playerInputHandler.OnInputDeviceChanged += OnInputDeviceChanged;
+            inputDevice = playerInputHandler.CurrentDevice;
+            
+            
+            Action = debugAction; // TODO Remove
+                
+            UpdateIcon();
+        }
+
+        private InputAction FindActionForActionName(string actionName)
+        {
+            if (!playerInputHandler)
+                return null;
+            
+            return playerInputHandler.GetInputAction(actionName);
         }
 
         private void OnInputDeviceChanged(string device)
         {
-            
+            inputDevice = device;
+            UpdateIcon();
+        }
+
+        private void UpdateIcon()
+        {
+            var icon = BindingsIconsUtil.GetSprite(action, inputDevice);
+            canvasGroup.alpha = icon ? 1f : 0f;
+            image.Sprite = icon;
         }
     }
 }
---
+++ Assets/_Scripts/Utilities/BindingsIconsUtil.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using System.Linq;
+using Scriptables;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Utilities
+{
+    public static class BindingsIconsUtil
+    {
+        private static GamepadIcons xbox;
+        private static GamepadIcons ps4;
+
+        private static void Init()
+        {
+            xbox = Resources.Load<GamepadIcons>("InputSystem/GamePadIcons_Xbox");
+            ps4 = Resources.Load<GamepadIcons>("InputSystem/GamePadIcons_PS");
+        }
+
+        public static Sprite GetSprite(string deviceLayoutName, string mainControlPath)
+        {
+            if (xbox == null) Init();
+
+            if (string.IsNullOrEmpty(deviceLayoutName) || string.IsNullOrEmpty(mainControlPath))
+                return null;
+
+            Sprite icon = default;
+            if (UnityEngine.InputSystem.InputSystem.IsFirstLayoutBasedOnSecond(deviceLayoutName, "DualShockGamepad"))
+            {
+                icon = ps4.GetSprite(mainControlPath);
+            }
+            else if (UnityEngine.InputSystem.InputSystem.IsFirstLayoutBasedOnSecond(deviceLayoutName, "Gamepad"))
+            {
+                icon = xbox.GetSprite(mainControlPath);
+            }
+
+            return icon;
+        }
+        
+        public static Sprite GetSprite(InputAction inputAction, string deviceName)
+        {
+            if (inputAction == null || string.IsNullOrEmpty(deviceName))
+                return null;
+            
+            var mainBindings = GetRelevantMainBindings(inputAction, deviceName);
+            if (!mainBindings.Any())
+                return null;
+
+            var mainBinding = mainBindings.First();
+
+            inputAction.GetBindingDisplayString(mainBinding, out var deviceLayoutName, out var mainControlPath);
+            
+            return GetSprite(deviceLayoutName, mainControlPath);
+        }
+
+        private static readonly List<int> relevantMainBindings = new List<int>(); // So we don't create an instance each time the next method is called
+        public static List<int> GetRelevantMainBindings(InputAction inputAction, string deviceName)
+        {
+            relevantMainBindings.Clear();
+            
+            if (inputAction.bindings[0].isComposite)
+            {
+                if (deviceName == "Gamepad")
+                {
+                    relevantMainBindings.Add(inputAction.bindings.Count - 2);
+                }
+                else
+                {
+                    for (int i = 1; i < inputAction.bindings.Count - 2; i += 2)
+                    {
+                        relevantMainBindings.Add(i);
+                    }
+                }
+            }
+            else
+            {
+                relevantMainBindings.Add(deviceName == "Gamepad" ? 2 : 0);
+            }
+
+            return relevantMainBindings;
+        }
+    }
+}
