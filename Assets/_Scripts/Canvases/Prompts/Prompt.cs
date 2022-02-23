47718fadf1fbeb0c67c33b21f81b47425012be79
William Lebel <william.leb@hotmail.com>
2022-02-22 22:14:01 -0500

    Add prompt that doesn't work

--- Assets/_Scripts/Canvases/Prompts/Prompt.cs
+++ Assets/_Scripts/Canvases/Prompts/Prompt.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using System;
+using InputSystem;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Canvases.Prompts
+{
+    [RequireComponent(typeof(PlayerInput))]
+    class Prompt : MonoBehaviour
+    {
+        private PlayerInput playerInput;
+        private DetectDevice detectDevice;
+
+        private void Awake()
+        {
+            playerInput = playerInput;
+        }
+
+        private void Start()
+        {
+            detectDevice = new DetectDevice(playerInput.currentActionMap);
+        }
+
+        private void OnEnable()
+        {
+            detectDevice.OnInputDeviceChanged += OnInputDeviceChanged;
+        }
+
+        private void OnDisable()
+        {
+            detectDevice.OnInputDeviceChanged -= OnInputDeviceChanged;
+        }
+
+        private void OnInputDeviceChanged(string device)
+        {
+            
+        }
+    }
+}
