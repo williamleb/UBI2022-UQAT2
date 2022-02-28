0b781d2055311f0050cef14d1cb73fcbed24b06b
William Lebel <william.leb@hotmail.com>
2022-02-28 15:31:43 -0500

    Restore teacher destination after being interupted

---
+++ Assets/_Scripts/Units/AI/Actions/InteruptableWait.cs
@@ -0,0 +1,48 @@
+﻿using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine;
+
+namespace Units.AI.Actions
+{
+    [TaskCategory("AI")]
+    [TaskIcon("{SkinColor}WaitIcon.png")]
+    public class InterruptibleWait : Wait
+    {
+        [BehaviorDesigner.Runtime.Tasks.Tooltip("Should the time the task passes when interrupted contribute to the wait time?")]
+        [SerializeField] private readonly SharedBool interruptionContributesToWaitTime = true;
+
+        private float endTime;
+
+        private bool hasFinished;
+        
+        public override void OnStart()
+        {
+            if (hasFinished)
+            {
+                base.OnStart();
+            }
+            else if (!interruptionContributesToWaitTime.Value)
+            {
+                StartTime += Time.time - endTime;
+            }
+            
+            hasFinished = false;
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            var result = base.OnUpdate();
+
+            if (result != TaskStatus.Running)
+                hasFinished = true;
+
+            return result;
+        }
+
+        public override void OnEnd()
+        {
+            base.OnEnd();
+            endTime = Time.time;
+        }
+    }
+}
