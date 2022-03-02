ea9537644f6656868f4b1792d8c7ccf3a6197003
William Lebel <william.leb@hotmail.com>
2022-03-01 20:18:37 -0500

    Add AI spawners

---
+++ Assets/_Scripts/Units/AI/AISpawnLocation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Fusion;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Units.AI
+{
+    public class AISpawnLocation : MonoBehaviour
+    {
+        [SerializeField, Required] private NetworkObject aiEntityPrefab;
+        [SerializeField, Required] private GameObject aiBrainPrefab;
+
+        [Header("Editor Only")]
+        [SerializeField] private Color gizmoColor;
+
+        public NetworkObject AIEntityPrefab => aiEntityPrefab;
+        public GameObject AIBrainPrefab => aiBrainPrefab;
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = gizmoColor;
+            
+            var position = transform.position;
+            Gizmos.DrawSphere(position, 0.5f);
+            Gizmos.DrawIcon(position + Vector3.up * 2f, "BuildSettings.Android", true, gizmoColor);
+        }
+    }
+}
---
+++ Assets/_Scripts/Units/AI/Conditionals/IsGameState.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using Managers.Game;
+using UnityEngine;
+
+namespace Units.AI.Conditionals
+{
+    [Serializable]
+    [TaskDescription("Returns success if the game state matches the chosen game state.")]
+    [TaskCategory("Game")]
+    public class IsGameState : Conditional
+    {
+        [SerializeField] private GameState gameState = GameState.Running;
+        [SerializeField] private SharedBool failOnNoGameManagerInstance = true;
+
+        public override TaskStatus OnUpdate()
+        {
+            if (!GameManager.HasInstance)
+                return failOnNoGameManagerInstance.Value ? TaskStatus.Failure : TaskStatus.Success;
+
+            return GameManager.Instance.CurrentState == gameState ? TaskStatus.Success : TaskStatus.Failure;
+        }
+
+        public override void OnReset()
+        {
+            base.OnReset();
+            gameState = GameState.Running;
+            failOnNoGameManagerInstance = true;
+        }
+    }
+}
