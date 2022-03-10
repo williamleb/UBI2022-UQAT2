26d00fb8d12819d078f4684f686b93f43ac27a03
William Lebel <william.leb@hotmail.com>
2022-03-10 14:10:29 -0500

    Add memory events without any parameters

--- Assets/_Scripts/Utilities/Event/MemoryEvent.cs
+++ Assets/_Scripts/Utilities/Event/MemoryEvent.cs
@@ -98,4 +98,92 @@ namespace Utilities.Event
             return newMemoryEvent;
         }
     }
+    
+    /// <summary>
+    /// Event that keeps previous events in memory to send to new subscribers.
+    /// </summary>
+    public struct MemoryEvent
+    {
+        private event Action OnEvent;
+        private int memory;
+
+        public MemoryEvent(MemoryEvent other)
+        {
+            OnEvent = default;
+            memory = other.memory;
+            
+            if (other.OnEvent != null)
+            {
+                foreach (var eventDelegate in other.OnEvent.GetInvocationList())
+                {
+                    OnEvent += (Action) eventDelegate;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Invoke the event, keeping the call in memory for future subscribers
+        /// </summary>
+        public void InvokeWithMemory()
+        {
+            memory++;
+            OnEvent?.Invoke();
+        }
+
+        /// <summary>
+        /// Invoke the event without keeping the call in memory
+        /// The future subscribers will never be notified of this call.
+        /// </summary>
+        public void InvokeWithoutMemory()
+        {
+            OnEvent?.Invoke();
+        }
+
+        /// <summary>
+        /// Clear the event's memory, preventing future subscriber from being notified of past calls.
+        /// </summary>
+        public void ClearMemory()
+        {
+            memory = 0;
+        }
+
+        /// <summary>
+        /// Remove a call from memory that will not be sent to future subscribers
+        /// </summary>
+        public void RemoveFromMemory()
+        {
+            memory = Math.Max(0, memory - 1);
+        }
+
+        private void AddAction(Action action)
+        {
+            for (var i = 0; i < memory; ++i)
+            {
+                action?.Invoke();
+            }
+            
+            OnEvent += action;
+        }
+        
+        private void RemoveAction(Action action)
+        {
+            OnEvent -= action;
+        }
+
+        public static MemoryEvent operator +(MemoryEvent memoryEvent, Action action)
+        {
+            var newMemoryEvent = new MemoryEvent(memoryEvent);
+            newMemoryEvent.AddAction(action);
+            
+            return newMemoryEvent;
+        }
+        
+        public static MemoryEvent operator -(MemoryEvent memoryEvent, Action action)
+        {
+            var newMemoryEvent = new MemoryEvent(memoryEvent);
+            newMemoryEvent.RemoveAction(action);
+            
+            return newMemoryEvent;
+        }
+    }
 }
