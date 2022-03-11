c22efbd6280c15bf6b157e27d118de2d61fed991
Martin <Martin.johnson.1@ens.etsmtl.ca>
2022-03-11 08:03:22 -0500

    changed controller to be network and removed from player

--- Assets/_Scripts/VFX/DustTrailController.cs
+++ Assets/_Scripts/VFX/DustTrailController.cs
@@ -1,17 +1,27 @@
 using System;
+using Fusion;
 using UnityEngine;
 
 namespace VFX
 {
-    public class DustTrailController : MonoBehaviour
+    public class DustTrailController : NetworkBehaviour
     {
         [SerializeField] private AnimationCurve interpolation;
+        [SerializeField] private Transform targetToFollow;
 
         [SerializeField] private ParticleSystem dustRound;
         [SerializeField] private ParticleSystem dustEdgy;
+
         [SerializeField] private ParticleSystemValues baseValues;
+
         //[SerializeField] private ParticleSystemValues midValues;
         [SerializeField] private ParticleSystemValues highValues;
+
+        private Vector3 offset;
+
+        private void Awake() => offset = transform.localPosition;
+
+        private void LateUpdate() => transform.position = targetToFollow.position - targetToFollow.forward + offset;
 
         public void UpdateDustTrail(float interpolationValue)
         {
@@ -28,13 +38,13 @@ namespace VFX
                 dustEdgy.Stop();
                 dustRound.Stop();
             }
-            
+
             float value = interpolation.Evaluate(Mathf.Clamp01(interpolationValue));
-            UpdateParticleSystem(dustEdgy, value, true);
-            UpdateParticleSystem(dustRound, value, false);
+            UpdateParticleSystem(dustEdgy, value, true, -targetToFollow.forward);
+            UpdateParticleSystem(dustRound, value, false, -targetToFollow.forward);
         }
 
-        private void UpdateParticleSystem(ParticleSystem ps, float lerpValue, bool isEdgy)
+        private void UpdateParticleSystem(ParticleSystem ps, float lerpValue, bool isEdgy, Vector3 direction)
         {
             var mainModule = ps.main;
             mainModule.startLifetime = Mathf.Lerp(baseValues.Lifetime, highValues.Lifetime, lerpValue);
@@ -43,20 +53,29 @@ namespace VFX
             mainModule.simulationSpeed = Mathf.Lerp(baseValues.SimulationSpeed, highValues.SimulationSpeed, lerpValue);
             var emissionModule = ps.emission;
             if (isEdgy)
-                emissionModule.rateOverDistance = Mathf.Lerp(baseValues.EmissionRateOverDistanceEdgy, highValues.EmissionRateOverDistanceEdgy, lerpValue);
+                emissionModule.rateOverDistance = Mathf.Lerp(baseValues.EmissionRateOverDistanceEdgy,
+                    highValues.EmissionRateOverDistanceEdgy, lerpValue);
             else
-                emissionModule.rateOverDistance = Mathf.Lerp(baseValues.EmissionRateOverDistanceRound, highValues.EmissionRateOverDistanceRound, lerpValue);
-            
+                emissionModule.rateOverDistance = Mathf.Lerp(baseValues.EmissionRateOverDistanceRound,
+                    highValues.EmissionRateOverDistanceRound, lerpValue);
+
             var shapeModule = ps.shape;
             shapeModule.angle = Mathf.Lerp(baseValues.Angle, highValues.Angle, lerpValue);
-            
+
             var lvolModule = ps.limitVelocityOverLifetime;
             lvolModule.dampen = Mathf.Lerp(baseValues.Dampen, highValues.Dampen, lerpValue);
 
             var folModule = ps.forceOverLifetime;
+            Vector3 baseMin = direction * baseValues.ForceLifetimeConstantXMin;
+            Vector3 baseMax = direction * baseValues.ForceLifetimeConstantXMax;
+            Vector3 highMin = direction * highValues.ForceLifetimeConstantXMin;
+            Vector3 highMax = direction * highValues.ForceLifetimeConstantXMax;
             folModule.x = new ParticleSystem.MinMaxCurve(
-                Mathf.Lerp(baseValues.ForceLifetimeConstantXMin,highValues.ForceLifetimeConstantXMin,lerpValue),
-                Mathf.Lerp(baseValues.ForceLifetimeConstantXMax,highValues.ForceLifetimeConstantXMax,lerpValue));
+                Mathf.Lerp(baseMin.x, highMin.x, lerpValue),
+                Mathf.Lerp(baseMax.x, highMax.x, lerpValue));
+            folModule.z = new ParticleSystem.MinMaxCurve(
+                Mathf.Lerp(baseMin.z, highMin.z, lerpValue),
+                Mathf.Lerp(baseMax.z, highMax.z, lerpValue));
         }
     }
 
@@ -73,6 +92,5 @@ namespace VFX
         public float Dampen;
         public float ForceLifetimeConstantXMin;
         public float ForceLifetimeConstantXMax;
-
     }
 }
