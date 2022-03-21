92354c0f09c617d8fb10db011bdaf4758a32ce2a
Martin Johnson <martinjohnson@live.ca>
2022-03-20 21:58:08 -0400

    start of prop generation

--- Assets/_Scripts/Systems/MapGeneration/MapGenerationInfo.cs
+++ Assets/_Scripts/Systems/MapGeneration/MapGenerationInfo.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Systems.MapGeneration
@@ -6,6 +8,31 @@ namespace Systems.MapGeneration
     public class MapGenerationInfo : MonoBehaviour
     {
         public IEnumerable<RoomGenerationInfo> Rooms => rooms;
+        public Transform PropParent => propParent;
+
+        [SerializeField] private Transform propParent;
         [SerializeField] private RoomGenerationInfo[] rooms;
+
+        [Button(ButtonSizes.Large)]
+        private void FindRoomsInPrefab()
+        {
+            rooms = GetComponentsInChildren<RoomGenerationInfo>();
+        }
+
+        [Button(ButtonSizes.Large)]
+        private void FindPropParent()
+        {
+            GameObject pp = GameObject.Find("Props");
+            if (pp)
+                propParent = pp.transform;
+            else
+                Debug.LogWarning("Need to create a Prop gameObject to hold all props");
+        }
+
+        private void OnValidate()
+        {
+            FindRoomsInPrefab();
+            FindPropParent();
+        }
     }
 }
