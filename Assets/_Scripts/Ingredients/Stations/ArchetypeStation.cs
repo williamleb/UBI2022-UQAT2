95eb35cd8913a2e665edb1136744a5b7fb17c6c2
Antoine Masse <info@antoinemasse.net>
2022-03-21 21:05:59 -0400

    Add archetype station (#90)

---
+++ Assets/_Scripts/Ingredients/Stations/ArchetypeStation.cs
@@ -0,0 +1,23 @@
+using Canvases.Markers;
+using Managers.Interactions;
+using Units.Player;
+using UnityEngine;
+
+public class ArchetypeStation : MonoBehaviour
+{
+    private Interaction interaction;
+    [SerializeField] private Archetype archetypeTypeStation;
+
+    private void Awake()
+    {
+        interaction = GetComponent<Interaction>();
+        interaction.OnInteractedWith += OnInteractedWith;
+        GetComponentInChildren<TextMarkerReceptor>().Text = $"Select {archetypeTypeStation}";
+    }
+
+    private void OnInteractedWith(Interacter interacter)
+    {
+        var playerEntity = interacter.GetComponent<PlayerEntity>();
+        playerEntity.AssignArchetype(archetypeTypeStation);
+    }
+}
