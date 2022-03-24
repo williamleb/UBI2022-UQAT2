944dfaa7b628ced7eb6f4aa2ae98aa87bef596ce
OrangeBender <martinjohnson@live.ca>
2022-03-24 13:44:05 -0400

    fixed always going left when moving up and down (#122)

--- Assets/_Scripts/Units/Player/PlayerMovement.cs
+++ Assets/_Scripts/Units/Player/PlayerMovement.cs
@@ -1,8 +1,11 @@
+using System;
+using System.Collections;
 using Fusion;
 using Systems.Network;
 using UnityEngine;
 using Utilities.Extensions;
 using VFX;
+using Random = UnityEngine.Random;
 
 namespace Units.Player
 {
@@ -25,6 +28,7 @@ namespace Units.Player
         private float velocity;
         private Vector3 lastMoveDirection = Vector3.zero;
         private bool isTurningAround;
+        private Coroutine rotateCoroutine;
         private bool HasMoveInput => MoveDirection.sqrMagnitude > 0.05;
         private bool IsMovingFast => velocity >= data.SprintFumbleThreshold * data.SprintMaximumSpeed;
 
@@ -38,7 +42,6 @@ namespace Units.Player
             HandleMoveInput(inputData);
             CalculateVelocity();
             MovePlayer();
-            RotatePlayer();
             dustTrailController.UpdateDustTrail(velocity/data.SprintMaximumSpeed, isRagdoll);
             UpdateCameraPointPosition();
         }
@@ -58,7 +61,16 @@ namespace Units.Player
             
             if (!IsDashing && !inMenu && !InCustomization)
             {
-                if (HasMoveInput) lastMoveDirection = MoveDirection;
+                if (HasMoveInput)
+                {
+                    if (lastMoveDirection != MoveDirection)
+                    {
+                        if (rotateCoroutine != null) StopCoroutine(rotateCoroutine);
+                        lastMoveDirection = MoveDirection;
+                        rotateCoroutine = StartCoroutine(RotatePlayerTowards());
+                    }
+                }
+
                 float directionDot = Vector3.Dot(transform.forward, lastMoveDirection);
                 if (directionDot <= -0.2 && velocity == 0) isTurningAround = true;
                 if (directionDot >= 0.99) isTurningAround = false;
@@ -113,17 +125,28 @@ namespace Units.Player
 
         private void MovePlayer()
         {
-            if (isTurningAround) return;
+            if (isTurningAround || isRagdoll) return;
             nRb.Rigidbody.velocity = transform.forward * velocity;
         }
 
-        private void RotatePlayer()
+        private IEnumerator RotatePlayerTowards()
         {
-            if (IsDashing) return;
-            float turnRateDivider = Mathf.Max(1, velocity - data.MoveMaximumSpeed);
-            float turnRate = data.TurnRotationSpeed * Runner.DeltaTime / turnRateDivider;
-            if (isTurningAround) turnRate *= 5;
-            transform.forward = Vector3.RotateTowards(transform.forward, lastMoveDirection, turnRate, 0);
+            if (IsDashing) yield break;
+
+            float angle = Vector3.SignedAngle(transform.forward, lastMoveDirection, Vector3.up);
+            if (Mathf.Abs(angle) > 179.5f) angle *= Random.Range(0, 2) * 2 - 1;
+            Vector3 startRot = transform.eulerAngles;
+            Vector3 endRot = startRot + Vector3.up * angle;
+            float t = 0;
+            while (t < 1)
+            {
+                float turnRateDivider = Mathf.Max(1, velocity - data.MoveMaximumSpeed);
+                float turnRate = data.TurnRotationSpeed * Runner.DeltaTime / (turnRateDivider);
+                if (isTurningAround) turnRate *= 5f;
+                t += turnRate;
+                transform.eulerAngles = Vector3.Lerp(startRot, endRot, t);
+                yield return null;
+            }
         }
 
         private void ResetVelocity()
