571bbda314a25366c8a2946f9aa012762f693fd6
William Lebel <william.leb@hotmail.com>
2022-03-24 22:16:52 -0400

    Add randomizer scripts for mesh and material (#128)

---
+++ Assets/_Scripts/Environment/Randomizer/MaterialRandomizer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Environment.Randomizer
+{
+    [RequireComponent(typeof(MeshRenderer))]
+    public class MaterialRandomizer : Randomizer
+    {
+        [SerializeField, PreviewField] private List<Material> possibleMaterials = new List<Material>();
+
+        private MeshRenderer meshRenderer;
+        
+        protected override int NumberOfElements => possibleMaterials.Count;
+
+        private void Awake()
+        {
+            meshRenderer = GetComponent<MeshRenderer>();
+        }
+
+        protected override void UpdateElement(int elementNumber)
+        {
+            meshRenderer.material = possibleMaterials[elementNumber];
+        }
+        
+#if UNITY_EDITOR
+        protected override void EditorUpdateElement(int elementNumber)
+        {
+            var editorMeshRenderer = GetComponent<MeshRenderer>();
+            editorMeshRenderer.material = possibleMaterials[elementNumber]; 
+        }
+#endif
+    }
+}
---
+++ Assets/_Scripts/Environment/Randomizer/Randomizer.cs
@@ -0,0 +1,72 @@
+﻿using Fusion;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Environment.Randomizer
+{
+    public abstract class Randomizer : NetworkBehaviour
+    {
+        [SerializeField] private bool randomizeAtStart = true;
+        
+        [Networked(OnChanged = nameof(OnCurrentElementIndexChanged)), HideInInspector] 
+        private int CurrentElementIndex { get; set; }
+        
+        protected abstract int NumberOfElements { get; }
+        protected abstract void UpdateElement(int elementNumber);
+
+        public override void Spawned()
+        {
+            base.Spawned();
+
+            if (Object.HasStateAuthority)
+            {
+                if (randomizeAtStart)
+                    Randomize();
+                
+                RPC_ForceUpdateAllClients();
+            }
+            
+            UpdateElement();
+        }
+
+        public void Randomize()
+        {
+            if (!Object.HasStateAuthority)
+                return;
+
+            if (NumberOfElements <= 0)
+                return;
+            
+            CurrentElementIndex = Random.Range(0, NumberOfElements);
+        }
+
+        [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+        private void RPC_ForceUpdateAllClients()
+        {
+            UpdateElement();
+        }
+
+        private void UpdateElement()
+        {
+            UpdateElement(CurrentElementIndex);
+        }
+
+        private static void OnCurrentElementIndexChanged(Changed<Randomizer> changed)
+        {
+            changed.Behaviour.UpdateElement();
+        }
+        
+#if UNITY_EDITOR
+        [Button("Randomize")]
+        private void EditorRandomize()
+        {
+            if (NumberOfElements <= 0)
+                return;
+            
+            EditorUpdateElement(Random.Range(0, NumberOfElements));
+        }
+
+        protected virtual void EditorUpdateElement(int elementNumber) { }
+#endif
+    }
+}
