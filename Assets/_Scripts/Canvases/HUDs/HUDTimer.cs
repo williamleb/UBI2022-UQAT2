fe0bba7dac089913876d5d8043ada60080ce2ad0
Antoine Masse <info@antoinemasse.net>
2022-03-25 10:28:48 -0400

    Add overtime (#138)
    * Add overtime
    
    * Fix score not going to 0

---
+++ Assets/_Scripts/Canvases/HUDs/HUDTimer.cs
@@ -0,0 +1,30 @@
+using Managers.Game;
+using System;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class HUDTimer : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+    private GameTimer gameTimer;
+
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+
+        if (GameManager.HasInstance)
+            gameTimer = GameManager.Instance.GameTimer;
+
+        Debug.Assert(gameTimer);
+    }
+
+    void Update()
+    {       
+        if (GameManager.Instance.CurrentState == GameState.Running || GameManager.Instance.CurrentState == GameState.Overtime)
+        {
+            var span = TimeSpan.FromSeconds(Convert.ToDouble(gameTimer.RemainingTime));
+            text.SetText($"{span.Minutes:00}:{span.Seconds:00}");
+        }
+    }
+}
