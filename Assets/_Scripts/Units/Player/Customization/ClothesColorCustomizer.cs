199dcd3221d48efd11261c22f385191fc801c579
William Lebel <william.leb@hotmail.com>
2022-03-26 02:05:21 -0400

    Fix customization for legs and shoes

--- Assets/_Scripts/Units/Player/Customization/ClothesColorCustomizer.cs
+++ Assets/_Scripts/Units/Player/Customization/ClothesColorCustomizer.cs
@@ -1,11 +1,14 @@
-﻿using UnityEngine;
+﻿using Systems.Settings;
+using UnityEngine;
 
 namespace Units.Player.Customisation
 {
     public class ClothesColorCustomizer : MaterialCustomizer
     {
         [SerializeField] private Archetype targetArchetype;
+        [SerializeField] private ClothesType clothesType;
 
         public Archetype TargetArchetype => targetArchetype;
+        public ClothesType ClothesType => clothesType;
     }
 }
