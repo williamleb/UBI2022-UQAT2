49e0f894609e42aa6b9259092179c757c109fc22
OrangeBender <martinjohnson@live.ca>
2022-03-29 02:41:54 -0400

    Added ragdoll to AI Student + pushable (#197)
    * Added ragdoll to AI student
    
    * Added pushable AI

--- Assets/_Scripts/Units/Player/PlayerInputHandler.cs
+++ Assets/_Scripts/Units/Player/PlayerInputHandler.cs
@@ -44,7 +44,7 @@ namespace Units.Player
                 PlayerInputAction = new PlayerInputAction();
                 detectDevice = new DetectDevice(PlayerInputAction.Player.Get());
                 RebindSaveLoad.LoadOverrides(PlayerInputAction.asset);
-                detectDevice.Finalize();
+                detectDevice.FinalizeInit();
                 EnableInput();
                 NetworkSystem.Instance.OnInputEvent += OnInput;
             }
