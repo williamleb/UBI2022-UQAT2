2146a3fe8da20172655c9589f530d71ee3a786d7
William Lebel <william.leb@hotmail.com>
2022-03-30 00:24:28 -0400

    Integrate sound for angry janitor

---
+++ Assets/_Scripts/Units/AI/Actions/Alert/PlayAlert.cs
@@ -0,0 +1,14 @@
+﻿using BehaviorDesigner.Runtime.Tasks;
+
+namespace Units.AI.Actions.Alert
+{
+    [TaskCategory("AI/Alert")]
+    public class PlayAlert : AIAction
+    {
+        public override TaskStatus OnUpdate()
+        {
+            Brain.PlayAlert();
+            return TaskStatus.Success;
+        }
+    }
+}
---
+++ Assets/_Scripts/Units/AI/Actions/Alert/StopAlert.cs
@@ -0,0 +1,14 @@
+﻿using BehaviorDesigner.Runtime.Tasks;
+
+namespace Units.AI.Actions.Alert
+{
+    [TaskCategory("AI/Alert")]
+    public class StopAlert : AIAction
+    {
+        public override TaskStatus OnUpdate()
+        {
+            Brain.StopAlert();
+            return TaskStatus.Success;
+        }
+    }
+}
