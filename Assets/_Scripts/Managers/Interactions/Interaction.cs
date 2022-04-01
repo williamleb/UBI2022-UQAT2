2203159addda17462d08920f560423dcee869552
William Lebel <william.leb@hotmail.com>
2022-04-01 01:41:14 -0400

    Fix some interactions in lobby

--- Assets/_Scripts/Managers/Interactions/Interaction.cs
+++ Assets/_Scripts/Managers/Interactions/Interaction.cs
@@ -96,9 +96,6 @@ namespace Managers.Interactions
 
             if (col && col.bounds.Contains(interacter.transform.position)) return true;
 
-            var one = ~(Layers.INTERACTION_MASK | Layers.AI_MASK | Layers.IGNORE_RAYCAST_MASK);
-            var two = interacter.LayerMask;
-            
             if (!Runner.GetPhysicsScene().Raycast(transform.position + Vector3.up,interacter.transform.position - transform.position, out hit, interacter.Radius, interacter.LayerMask))
                 return false;
             
