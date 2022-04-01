22b47ae9fe03f043c535608d8a76f9ea0c745f8f
William Lebel <william.leb@hotmail.com>
2022-04-01 11:52:47 -0400

    Add tuto text in lobby (#250)
    * Add tuto text in lobby
    
    * updated tutorial
    
    Co-authored-by: Jordan Dion-Duval <dioj29@uqat.ca>

---
+++ Assets/_Scripts/Ingredients/Volumes/TextVolume.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Canvases.Markers;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Ingredients.Volumes
+{
+    [RequireComponent(typeof(LocalPlayerDetection))]
+    public class TextVolume : MonoBehaviour
+    {
+        [SerializeField] private TextMarkerReceptor textMarker;
+
+        private LocalPlayerDetection detection;
+
+        private void Awake()
+        {
+            detection = GetComponent<LocalPlayerDetection>();
+        }
+
+        private void Start()
+        {
+            detection.OnLocalPlayerEntered += Activate;
+            detection.OnLocalPlayerLeft += Deactivate;
+        }
+
+        private void OnDestroy()
+        {
+            detection.OnLocalPlayerEntered -= Activate;
+            detection.OnLocalPlayerLeft -= Deactivate;
+        }
+
+        private void Activate()
+        {
+            if (textMarker) textMarker.Activate();
+        }
+
+        private void Deactivate()
+        {
+            if (textMarker) textMarker.Deactivate();
+        }
+    }
+}
