8395f6f3afedaf4d3ee4d465e1fa4132f101fa59
Antoine Masse <info@antoinemasse.net>
2022-04-02 19:10:03 -0400

    UI Feedback on team score

---
+++ Assets/_Scripts/Canvases/HUDs/HomeworkDefinitionToVFX.cs
@@ -0,0 +1,9 @@
+using Ingredients.Homework;
+using UnityEngine;
+
+public class HomeworkDefinitionToVFX : MonoBehaviour
+{
+    [SerializeField] public GameObject leftVfx;
+    [SerializeField] public GameObject rightVfx;
+    [SerializeField] public HomeworkDefinition Definition;
+}
