9ae89ab868dea4a5440d0d552f91c04e6be2e0a9
William Lebel <william.leb@hotmail.com>
2022-04-02 17:37:07 -0400

    Change to menu music on game end

---
+++ Assets/_Scripts/Managers/Game/ChangeMusicStateOnGameEnd.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Systems.Sound;
+using UnityEngine;
+
+namespace Managers.Game
+{
+    public class ChangeMusicStateOnGameEnd : MonoBehaviour
+    {
+        private void Start()
+        {
+            if (GameManager.HasInstance)
+                GameManager.Instance.OnGameStateChanged += OnGameStateChanged;
+        }
+
+        private void OnDestroy()
+        {
+            if (GameManager.HasInstance)
+                GameManager.Instance.OnGameStateChanged -= OnGameStateChanged;
+        }
+
+        private void OnGameStateChanged(GameState gameState)
+        {
+            if (gameState == GameState.Finished)
+            {
+                if (SoundSystem.HasInstance) SoundSystem.Instance.SetInMenu();
+            }
+        }
+    }
+}
