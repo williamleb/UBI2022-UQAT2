be26c05a522da77bf007721a0e78a6271cadf97d
Antoine Masse <info@antoinemasse.net>
2022-04-04 19:43:13 -0400

    Add session name sequence in lobby (#274)
    * Add session name sequence in lobby
    
    Created prefab for readyup and countdown
    
    * Missing reference to new prefab
    
    * null checking

---
+++ Assets/_Scripts/Canvases/HUDs/HUDSessionSequence.cs
@@ -0,0 +1,40 @@
+using Canvases.Matchmaking;
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using System.Linq;
+using Systems.Network;
+using UnityEngine;
+
+public class HUDSessionSequence : MonoBehaviour
+{
+    List<string> sessionName;
+    [SerializeField, Required] private List<HostJoinSequenceElement> elements;
+
+    void Start()
+    {
+        if(NetworkSystem.Instance.NetworkRunner == null || NetworkSystem.Instance.NetworkRunner.SessionInfo == null || NetworkSystem.Instance.NetworkRunner.SessionInfo.Name == null)
+        {
+            Debug.LogWarning("Error retreiving session info. The lobby name will not be displayed.");
+            return;
+        }
+
+        sessionName = NetworkSystem.Instance.NetworkRunner.SessionInfo.Name.Split('-').ToList();
+
+        if (!elements.Any() || sessionName == null)
+        {
+            Debug.LogWarning("Missing components. The lobby name will not be displayed.");
+            return;
+        }
+
+        if (elements.Count < sessionName.Count)
+        {
+            Debug.LogWarning("The lobby name is larger than the number of sequence elements in the UI. The lobby name will not be displayed.");
+            return;
+        }
+
+        for (int i = 0; i < sessionName.Count; i++)
+        {
+            elements[i].CurrentValue = int.Parse(sessionName[i]);
+        }
+    }
+}
